9820a6b900095285735e289abd6118d1cd8eaa50
Ajay2810-hub <54299482+Ajay2810-hub@users.noreply.github.com>
2020-02-14 15:41:49 +0530

    Update coty.py

--- coty.py
+++ coty.py
@@ -3,17 +3,25 @@ import shutil
 from tkinter import *
 from tkinter import messagebox
 
+#Creating the Main Window
+
 win=Tk()
 win.geometry("500x500")
 win.title("COTY")
 win["bg"]='#fff'
 
+# Defining a function to show the copy section
+
 def copy():
     copy1()
+
+# Creating a Copy Section button
 
 copy_btn=Button(win,text="COPY",fg="#fff",bg='crimson',bd=0,activeforeground="yellow",activebackground="lightblue",command=copy)
 copy_btn.config(font="helvetica 20")
 copy_btn.place(x=200,y=100)
+
+#Same for Move section
 
 def move():
     move1()
@@ -22,6 +30,8 @@ move_btn=Button(win,text="MOVE",fg="#fff",bg='lightgreen',bd=0,activeforeground=
 move_btn.config(font="helvetica 20")
 move_btn.place(x=200,y=200)
 
+#Same for Delete section
+
 def delete():
     delete1()
     
@@ -29,35 +39,57 @@ delete_btn=Button(win,text="Delete",fg="#fff",bg='#0080ff',bd=0,activeforeground
 delete_btn.config(font="helvetica 20")
 delete_btn.place(x=200,y=300)
 
+#Creating the Filename label
+
 fname=Label(win,text="Filename",bg="#fff")
 fname.config(font="helvetica 20")
+
+#Entry for Filename
 
 fname1=Entry(win,bg="#fff",fg="crimson")
 fname1.config(font="helvetica 20")
 
+#Label for Destination
+
 des=Label(win,text="Destination",bg="#fff")
 des.config(font="helvetica 20")
+
+#Entry for Destination
 
 des1=Entry(win,bg="#fff",fg="crimson")
 des1.config(font="helvetica 20")
 
+#Calling the Copy Function
+
 def copy2():
     copy3()
-    
+
+#Button for copying
+
 copy_btn1=Button(win,text="COPY",fg="#fff",bg='crimson',bd=0,activeforeground="yellow",activebackground="lightblue",command=copy2)
 copy_btn1.config(font="helvetica 20")
+
+#Creating the label for filename
 
 fname2=Label(win,text="Filename",bg="#fff")
 fname2.config(font="helvetica 20")
 
+#Entry for filename
+
 fname3=Entry(win,bg="#fff",fg="crimson")
 fname3.config(font="helvetica 20")
+
+#Label for destination
 
 des2=Label(win,text="Destination",bg="#fff")
 des2.config(font="helvetica 20")
 
+#Entry for destination
+
 des3=Entry(win,bg="#fff",fg="crimson")
 des3.config(font="helvetica 20")
+
+#Moving Function
 
 def move2():
     move3()
@@ -65,11 +97,17 @@ def move2():
 move_btn1=Button(win,text="MOVE",fg="#fff",bg='lightgreen',bd=0,activeforeground="#fff",activebackground="green",command=move2)
 move_btn1.config(font="helvetica 20")
 
+#Label for filename
+
 fname4=Label(win,text="Filename",bg="#fff")
 fname4.config(font="helvetica 20")
 
+#Entry for filename
+
 fname5=Entry(win,bg="#fff",fg="crimson")
 fname5.config(font="helvetica 20")
+
+#Deleting Function
 
 def delete2():
     delete3()
@@ -77,11 +115,15 @@ def delete2():
 delete_btn1=Button(win,text="Delete",fg="#fff",bg='#0080ff',bd=0,activeforeground="crimson",activebackground="yellow",command=delete2)
 delete_btn1.config(font="helvetica 20")
 
+#Back button and it's command
+
 def back():
     back1()
     
 backbtn=Button(win,text="<",bg="crimson",fg="#fff",bd=0,width=2,activebackground="#fff",activeforeground="crimson",command=back)
 backbtn.config(font="helvetica 20")
+
+#Copy Section placements
 
 def copy1():
     copy_btn.place(x=-1000,y=100)
@@ -96,6 +138,8 @@ def copy1():
     backbtn.place(x=5,y=5)
     backbtn.config(fg="white",bg="crimson",activebackground="#fff",activeforeground="crimson")
 
+#Move Section placements
+
 def move1():
     copy_btn.place(x=-1000,y=100)
     move_btn.place(x=-1000,y=200)
@@ -109,6 +153,8 @@ def move1():
     backbtn.place(x=5,y=5)
     backbtn.config(fg="white",bg="lightgreen",activebackground="#fff",activeforeground="lightgreen")
 
+#Delete Section placements
+
 def delete1():
     copy_btn.place(x=-1000,y=100)
     move_btn.place(x=-1000,y=200)
@@ -119,6 +165,8 @@ def delete1():
     delete_btn1.place(x=200,y=200)
     backbtn.place(x=5,y=5)
     backbtn.config(fg="white",bg="#0080ff",activebackground="#fff",activeforeground="#0080ff")
+
+#Copy function
 
 def copy3():
     file=str(fname1.get())
@@ -131,6 +179,8 @@ def copy3():
     else:
        messagebox.showerror("COTY","File Doesn't Exists!!")
 
+#Move function
+
 def move3():
     file=str(fname3.get())
     dest=str(des3.get())
@@ -141,13 +191,17 @@ def move3():
           messagebox.showerror("COTY","Invalid Destination!!")
     else:
        messagebox.showerror("COTY","File Doesn't Exists!!")
-      
+
+#Delete Function
+
 def delete3():
     file=str(fname5.get())
     if os.path.exists(f"{file}"):
        os.remove(file)
     else:
        messagebox.showerror("COTY","File Doesn't Exists!!")
+
+#Back Button Function
 
 def back1():
     copy_btn.place(x=200,y=100)
@@ -178,3 +232,4 @@ def back1():
     des3.delete(first=0,last=dele1)
     dele1=len(fname5.get())
     fname5.delete(first=0,last=dele1)
+
